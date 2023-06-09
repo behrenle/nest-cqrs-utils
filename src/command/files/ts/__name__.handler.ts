1376e683d1ded0428ef30ccbfc237e120c78ff53
Lennard Behrens <lb@thepeaklab.com>
2023-06-09 14:58:34 +0200

    fix: fix command handler template

--- src/command/files/ts/__name__.handler.ts
+++ src/command/files/ts/__name__.handler.ts
@@ -2,8 +2,8 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { <%= classify(name) %>Command } from './<%= name %>.resolver';
 import { <%= classify(name) %>ResponseDto } from './<%= name %>.response.dto';
 
-@CommandHandler()
-export class <%= classify(name)%>Command implements ICommandHandler<<%= classify(name) %>Command> {
+@CommandHandler(<%= classify(name)%>Command)
+export class <%= classify(name)%>Handler implements ICommandHandler<<%= classify(name) %>Command> {
   async execute(
     command: <%= classify(name) %>Command,
   ): Promise<<%= classify(name) %>ResponseDto> {
